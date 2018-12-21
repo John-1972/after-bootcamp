159da696d7c42ebfb17fdbb73308d857d61f6fa3
John Henderson <johnhenderson@fastmail.fm>
2018-12-21 12:07:27 +0000

    Bootstrap and dependencies added to Gemfile

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -5,5 +5,6 @@ class LinksController < ApplicationController
   def advice
     @skill_links = Link.where(section: :skills) # All 'skill' entries from DB --> ONE instance variable
     @wage_links = Link.where(section: :pay_rates) # All 'pay_rates' entries in DB --> ONE instance variable
+    @jobsearch_links = Link.where(section: :jobsearch_advice) # All 'jobsearch_advice' DB entries --> ONE var
   end
 end
