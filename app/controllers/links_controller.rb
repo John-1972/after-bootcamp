820ef3221da4ac1a0703e4b0972ce9e0c26bbabf
John Henderson <johnhenderson@fastmail.fm>
2018-12-24 16:52:41 +0000

    Added job_ops to routes and links controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -6,5 +6,14 @@ class LinksController < ApplicationController
     @skill_links = Link.where(section: :skills) # All 'skill' entries from DB --> ONE instance variable
     @wage_links = Link.where(section: :pay_rates) # All 'pay_rates' entries in DB --> ONE instance variable
     @jobsearch_links = Link.where(section: :jobsearch_advice) # All 'jobsearch_advice' DB entries --> ONE var
+    @resume_links = Link.where(section: :resume_portfolio)
+    @interview_links = Link.where(section: :interview_prep)
+    @data_algo_links = Link.where(section: :data_struct_algo)
+    @question_links = Link.where(section: :interview_quests)
+    @youtube_links = Link.where(section: :youtube_channels)
+  end
+
+  def job_ops
+    @international_links = Link.where(section: :int_job_ops) # All 'int_job_ops' links in DB --> ONE inst var
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'links#index'
 
   get 'advice', to: 'links#advice'
+  get 'job_ops', to: 'links#job_ops'
+  get 'courses', to: 'links#courses'
 end
