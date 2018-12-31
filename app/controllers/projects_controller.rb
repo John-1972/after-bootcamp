df666a7f317611ae7877a71323a5abadcaae30d8
John Henderson <johnhenderson@fastmail.fm>
2018-12-31 19:24:46 +0000

    Code challenge websites added to Projects index page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
   def index
+    @coding_site_links = Link.where(section: :coding_site) # Can my Projects controller pull links from the DB?
   end
 
   def planets
