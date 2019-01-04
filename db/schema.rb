8ea0ca4f31ca5e636a15d382f830720bdee1516e
John Henderson <johnhenderson@fastmail.fm>
2019-01-04 20:48:24 +0000

    Created a model/table for headings, associated them with links

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181204154020) do
+ActiveRecord::Schema.define(version: 20190104203924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "headings", force: :cascade do |t|
+    t.string   "heading_text"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "links", force: :cascade do |t|
     t.string   "section"
