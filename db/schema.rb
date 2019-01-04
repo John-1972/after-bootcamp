4759175fc05ca7f1fda0252830d40fb374cca3dc
John Henderson <johnhenderson@fastmail.fm>
2019-01-04 21:08:00 +0000

    Added a column to the links table to store the heading_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190104203924) do
+ActiveRecord::Schema.define(version: 20190104210317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20190104203924) do
     t.string   "URL"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "heading_id"
   end
 
 end
