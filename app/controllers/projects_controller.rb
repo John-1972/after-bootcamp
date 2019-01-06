b337be3848545641f0cc80af82f2541ba7253829
John Henderson <johnhenderson@fastmail.fm>
2019-01-06 22:40:55 +0000

    Heading and Links on Project index page now calculated from heading_id

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,12 @@
 class ProjectsController < ApplicationController
   def index
-    @coding_site_links = Link.where(section: :coding_site) # My Projects controller can pull links from the DB!
+    # The title of the section containing the code challenge websites is stored in the 'Heading' table in
+    # the column called 'heading_text' with an 'id' of 1.
+    @coding_site_heading = Heading.where(id: 1)
+    
+    # The links for that section are stored in the 'Link' table, and also have a value of 1 in the
+    # 'heading_id' column.
+    @coding_site_links = Link.where(heading_id: 1) # My Projects controller can pull links from the DB!
   end
 
   def planets
