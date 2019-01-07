eeabacd872a87682ab89c923a26eff1b2af33b35
John Henderson <johnhenderson@fastmail.fm>
2019-01-07 12:28:31 +0000

    Refactored the code in my Projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,12 +1,18 @@
 class ProjectsController < ApplicationController
   def index
+    # All section headings are stored in the 2-column 'Heading' table. One column is 'id' and the other is
+    # 'heading_text'.
+
+    # The 'id' for the heading at the bottom of the Projects index page is 1.
+    this_heading = 1
+
     # The title of the section containing the code challenge websites is stored in the 'Heading' table in
     # the column called 'heading_text' with an 'id' of 1.
-    @coding_site_heading = Heading.where(id: 1)
+    @challenge_site_heading = Heading.where(id: this_heading)
     
-    # The links for that section are stored in the 'Link' table, and also have a value of 1 in the
+    # The links for that section are stored in the 'Link' table, and have a matching value in the
     # 'heading_id' column.
-    @coding_site_links = Link.where(heading_id: 1) # My Projects controller can pull links from the DB!
+    @challenge_site_links = Link.where(heading_id: this_heading)
   end
 
   def planets
