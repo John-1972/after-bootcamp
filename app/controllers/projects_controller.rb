3e29f0a79902135e5d13376552cf6885de5a5fa4
John Henderson <johnhenderson@fastmail.fm>
2024-05-03 17:21:08 +0000

    Minor alterations to code & page text, added comments to clarify code

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,12 +3,10 @@ class ProjectsController < ApplicationController
     # All section headings are stored in the 2-column 'Heading' table. One column is 'id' and the other is
     # 'heading_text'.
 
-    # The 'id' for the 'Coding Challenge Sites' heading at the bottom of the Projects index page is 1.
+    # The heading text for the section containing the code challenge websites is stored in the
+    # 'Heading' table. It's the 1st row and so has an 'id' of 1.
     this_heading = 1
-
-    # The title of the section containing the code challenge websites is stored in the 'Heading' table in
-    # the column called 'heading_text' with an 'id' of 1.
-    @challenge_site_heading = Heading.find(this_heading)
+    @challenge_site_headings = Heading.find(this_heading)
     
     # The links for that section are stored in the 'Link' table, and have a matching value in the
     # 'heading_id' column.
